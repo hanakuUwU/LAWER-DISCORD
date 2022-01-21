db22d4e37495d156b58a2c3c5a97843a201ae697
TanDungNguyenProleaker =)) <95019075+tdunguwu@users.noreply.github.com>
2022-01-21 20:49:11 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,39 @@
+{
+  "name": "lawer-bot",
+  "version": "BETA",
+  "description": "Bot discord made by tdunguwu",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index"
+  },
+  "author": "KIRA",
+  "license": "MIT",
+  "dependencies": {
+    "ascii-table": "0.0.9",
+    "authenticator": "^1.1.5",
+    "axios": "^0.24.0",
+    "canvacord": "^5.2.3",
+    "chalk": "^4.1.2",
+    "common-tags": "^1.8.2",
+    "discord-image-generation": "^1.4.9",
+    "discord-music-player": "^8.3.1",
+    "discord.js": "12.5.3",
+    "express": "^4.17.2",
+    "fs-extra": "^10.0.0",
+    "glob": "^7.2.0",
+    "google-tts-api": "^2.0.2",
+    "moment": "^2.29.1",
+    "mongoose": "^6.1.7",
+    "ms": "^2.1.3",
+    "node-fetch": "^3.1.1",
+    "quick.db": "^7.1.3",
+    "semver": "^7.3.5",
+    "something-random-on-discord": "^3.4.4",
+    "srod-v2": "^1.0.2",
+    "twemoji-parser": "^13.1.0",
+    "youtube-sr": "^4.1.11",
+    "yt-search": "^2.10.3",
+    "ytdl-core": "^4.10.0",
+    "ytsr": "^3.6.0"
+  }
+}
